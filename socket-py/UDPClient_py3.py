eb387383d5d50916e5b2fc04429a77bafafdf79e
Chengqi Guan <glxh.git@gmail.com>
2016-07-23 20:48:38 +0800

    socket

--- socket-py/UDPClient_py3.py
+++ socket-py/UDPClient_py3.py
@@ -2,8 +2,8 @@ from socket import *
 serverName = "115.28.188.45"
 serverPort = 12000
 clientSocket = socket(AF_INET,SOCK_DGRAM)
-message = raw_input('Inpot lowercase sentence:')
+message = input('Inpot lowercase sentence:')
 clientSocket.sendto(message,(serverName,serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
+printf(modifiedMessage)
 clientSocket.close()
